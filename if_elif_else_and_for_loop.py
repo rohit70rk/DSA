b9fb4e6aef16c3021a7c4172e67cc9846c9aa8af
rohit70r <rohitkumar81953@gmail.com>
2025-09-17 22:27:31 +0530

    first commit

---
+++ if_elif_else_and_for_loop.py
@@ -0,0 +1,8 @@
+list = [74,84,28,73,82]
+for i in list:
+    if i > 80:
+        print(f"{i} is greater than 80")
+    elif i < 80:
+        print(f"{i} is less than 80")
+    else:
+        print(f"{i} is equal to 80")
