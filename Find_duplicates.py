f66261f99cf165adb44b626786a0c9d7229452b6
rohit70r <rohitkumar81953@gmail.com>
2025-09-19 01:42:55 +0530

    made some changes

--- Find_duplicates.py
+++ Find_duplicates.py
@@ -5,26 +5,31 @@ def findDuplicates(arr):
   
     # Step 1: Create an empty dictionary
     # to store element frequencies
+
     freqMap = {}
     result = []
 
     # Step 2: Iterate through the array and
     # count element frequencies
+
     for num in arr:
         freqMap[num] = freqMap.get(num, 0) + 1
 
     # Step 3: Iterate through the dictionary to
     # find duplicates
+
     for key, value in freqMap.items():
         if value > 1:
             result.append(key)
 
     # Step 4: If no duplicates found, add -1 to the result
+
     if not result:
         result.append(-1)
 
     # Step 6: Return the result list containing
     # duplicate elements or -1
+    
     return result
 
 
